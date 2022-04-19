00286aabe55abafc118739c3145efac93ebf1c91
Abdelrahmanm22 <abdra1396@gmail.com>
2022-04-20 01:32:11 +0200

    seven

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+function login(){
+    var e =document.getElementById("email").value;
+    var p = document.getElementById("pass").value;
+    
+    if(e=="nouran" && p=="01145062539")
+    {
+        location.href="";
+    }
+    
+}
